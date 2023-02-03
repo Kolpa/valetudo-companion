eb581ca8285b8259bf0b59c793059779dec31954
Kolya Opahle <kolyaopahle@gmail.com>
2023-02-03 11:47:18 +0100

    newlines everywhere

--- app/src/main/java/cloud/valetudo/companion/utils/ContextUtils.kt
+++ app/src/main/java/cloud/valetudo/companion/utils/ContextUtils.kt
@@ -3,7 +3,6 @@ package cloud.valetudo.companion.utils
 import android.widget.TextView
 import androidx.annotation.StringRes
 
-
 fun TextView.setText(@StringRes resId: Int, vararg vs: Any) {
     text = context.getString(resId, *vs)
 }
